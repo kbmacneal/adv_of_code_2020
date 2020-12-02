fde02bc7fe4d354072ac3d939b98ee0c59634901
Ken MacNeal <kenneth.b.macneal@gmail.com>
2020-12-02 07:56:48 -0600

    even smaller!

--- days/2.cs
+++ days/2.cs
@@ -16,15 +16,22 @@ namespace adv_of_code_2020
             public string value { get; set; }
             public Boolean isValid => value.Where(e => e == letter).Count() <= max && value.Where(e => e == letter).Count() >= min;
             public Boolean isPart2Valid => value[min - 1] == letter ^ value[max - 1] == letter;
+
+            public password(string line)
+            {
+                string[] split = line.Split(' ');
+                min = Int32.Parse(split[0].Split('-')[0]);
+                max = Int32.Parse(split[0].Split('-')[1]);
+                letter = split[1][0];
+                value = split[2];
+            }
         }
 
         public static async Task<string> Run()
         {
             StringBuilder answer = new StringBuilder();
 
-            List<password> passwords = new List<password>();
-
-            System.IO.File.ReadAllLines("inputs\\2.txt").ToList().ForEach(e => passwords.Add(parse_line(e)));
+            List<password> passwords = System.IO.File.ReadAllLines("inputs\\2.txt").Select(e => new password(e)).ToList();
 
             answer.AppendLine("Part 1: " + passwords.Where(e => e.isValid).Count());
 
@@ -32,21 +39,5 @@ namespace adv_of_code_2020
 
             return answer.ToString();
         }
-
-        private static password parse_line(string line)
-        {
-            string[] split = line.Split(' ');
-
-            password p = new password()
-            {
-                min = Int32.Parse(split[0].Split('-')[0]),
-                max = Int32.Parse(split[0].Split('-')[1]),
-                letter = split[1][0],
-                value = split[2]
-            };
-
-            return p;
-        }
-
     }
 }
