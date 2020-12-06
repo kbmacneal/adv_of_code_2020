80feb163fdfed3d8da6ccbc7155d40b5b0b45415
Ken MacNeal <kenneth.b.macneal@gmail.com>
2020-12-06 13:51:45 -0600

    condensing solution for day 6

--- days/6.cs
+++ days/6.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,32 +10,17 @@ namespace adv_of_code_2020
         public string Part1Answer { get; set; }
         public string Part2Answer { get; set; }
 
-
         public async Task Run()
         {
             string input = await File.ReadAllTextAsync("inputs\\6.txt");
 
             var groups = input.Split("\n\n").Select(e => e.Replace("\n", ""));
 
-            int count = 0;
+            Part1Answer = groups.Select(e => e.Distinct().Count()).Sum().ToString();
 
-            foreach(var group in groups)
-            {
-                count += group.Distinct().Count();
-            }
+            var group_expanded = input.Split("\n\n").Select(e => e.Split("\n"));
 
-            Part1Answer = count.ToString();
-
-            count = 0;
-
-            var group_expanded = input.Split("\n\n").Select(e=>e.Split("\n"));
-
-            foreach (var group in group_expanded)
-            {
-                count += group.Aggregate(new HashSet<char>(group.First()), (h, e) => { h.IntersectWith(e); return h; }).Count();
-            }
-
-            Part2Answer = count.ToString();
+            Part2Answer = group_expanded.Select(x => x.Aggregate(new HashSet<char>(x.First()), (h, e) => { h.IntersectWith(e); return h; }).Count()).Sum().ToString();
         }
     }
 }
