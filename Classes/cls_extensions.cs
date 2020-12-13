99ac267f51b99e6354778e4f9d12ce5d76cb30ef
Ken MacNeal <kenneth.b.macneal@gmail.com>
2020-12-13 09:08:06 -0600

    math is o so much fun

--- Classes/cls_extensions.cs
+++ Classes/cls_extensions.cs
@@ -120,6 +120,16 @@ namespace adv_of_code_.Classes
             return points.Where(e => origin.Around(diagonals).ToList().Contains(e)).ToList();
         }
 
+        public static int NextMultipleAfter(this int n, int target)
+        {
+            return (n - (target % n)) + target;
+        }
+
+        public static int AbsDifference(this int n, int target)
+        {
+            return Math.Abs(target - n);
+        }
+
         //from https://github.com/sjmulder/aoc/blob/master/2020/day01-cs-combine/Program.cs
         private static IEnumerable<IEnumerable<T>> Combine<T>(this T[] xs, int n)
         {
@@ -137,6 +147,66 @@ namespace adv_of_code_.Classes
 
         //from https://www.geeksforgeeks.org/lcm-of-given-array-elements/
         public static long GetLCM(this int[] element_array)
+        {
+            long lcm_of_array_elements = 1;
+            int divisor = 2;
+
+            while (true)
+            {
+                int counter = 0;
+                bool divisible = false;
+                for (int i = 0; i < element_array.Length; i++)
+                {
+                    // lcm_of_array_elements (n1, n2, ... 0) = 0.
+                    // For negative number we convert into
+                    // positive and calculate lcm_of_array_elements.
+                    if (element_array[i] == 0)
+                    {
+                        return 0;
+                    }
+                    else if (element_array[i] < 0)
+                    {
+                        element_array[i] = element_array[i] * (-1);
+                    }
+                    if (element_array[i] == 1)
+                    {
+                        counter++;
+                    }
+
+                    // Divide element_array by devisor if complete
+                    // division i.e. without remainder then replace
+                    // number with quotient; used for find next factor
+                    if (element_array[i] % divisor == 0)
+                    {
+                        divisible = true;
+                        element_array[i] = element_array[i] / divisor;
+                    }
+                }
+
+                // If divisor able to completely divide any number
+                // from array multiply with lcm_of_array_elements
+                // and store into lcm_of_array_elements and continue
+                // to same divisor for next factor finding.
+                // else increment divisor
+                if (divisible)
+                {
+                    lcm_of_array_elements = lcm_of_array_elements * divisor;
+                }
+                else
+                {
+                    divisor++;
+                }
+
+                // Check if all element_array is 1 indicate
+                // we found all factors and terminate while loop.
+                if (counter == element_array.Length)
+                {
+                    return lcm_of_array_elements;
+                }
+            }
+        }
+
+        public static long GetLCM(this long[] element_array)
         {
             long lcm_of_array_elements = 1;
             int divisor = 2;
---
+++ days/13.cs
@@ -0,0 +1,63 @@
+using adv_of_code_.Classes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adv_of_code_2020
+{
+    public class Day13 : IDay
+    {
+        public string Part1Answer { get; set; } = "";
+        public string Part2Answer { get; set; } = "";
+        public Stopwatch sw { get; set; } = new Stopwatch();
+
+        private class bus
+        {
+            public int id { get; set; }
+            public int target { get; set; }
+            public int nextMultipleAfter => this.id.NextMultipleAfter(target);
+
+            public bus(int ID, int Target)
+            {
+                id = ID;
+                target = Target;
+            }
+        }
+
+        public async Task Run()
+        {
+            string[] input = await File.ReadAllLinesAsync("inputs\\13.txt");
+
+            var target = Int32.Parse(input[0]);
+
+            List<bus> busses = input[1].Split(',').Where(e => e != "x").Select(Int32.Parse).Select(e => new bus(e, target)).ToList();
+
+            Part1Answer = (busses.Min(e => e.nextMultipleAfter).AbsDifference(target) * busses.OrderBy(e => e.nextMultipleAfter).First().id).ToString();
+
+            string[] times = input[1].Split(",");
+
+            var earliestTime = long.Parse(times[0]);
+            var increment = earliestTime;
+            for (int i = 1; i < times.Length; i++)
+            {
+                if (times[i] == "x") continue;
+
+                var curTime = long.Parse(times[i]);
+
+                var modValue = curTime - (i % curTime);
+
+                while (earliestTime % curTime != modValue)
+                {
+                    earliestTime += increment;
+                }
+
+                increment = new long[] { increment, curTime }.GetLCM();
+            }
+
+            Part2Answer = earliestTime.ToString();
+        }
+    }
+}
